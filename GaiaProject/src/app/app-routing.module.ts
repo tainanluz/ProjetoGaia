56fc4e8b18a882827b00bf04c7fd2a6b1f5480b9
snaiter0 <Douglas.szlps@gmail.com>
2021-09-28 12:38:56 -0300

    [feature Doug - Task: add Routs]

--- GaiaProject/src/app/app-routing.module.ts
+++ GaiaProject/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'home', pathMatch:'full'},
+  {path: 'home', component:HomeComponent},
+  {path: 'aboutUs', component:AboutUsComponent},
+  {path: 'contact', component:ContactComponent},
+  {path: 'footer', component:FooterComponent},
+  {path: 'menu', component:MenuComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
