84661d836501d30609f07d1520c4be2c93cd7c76
snaiter0 <Douglas.szlps@gmail.com>
2021-09-28 22:52:38 -0300

    [feature Leo - Task: contact component]

--- GaiaProject/src/app/contact/contact.component.ts
+++ GaiaProject/src/app/contact/contact.component.ts
@@ -5,11 +5,32 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
+
+
+
 export class ContactComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+
+    (function () {
+      'use strict';
+      window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+          form.addEventListener('submit', function (event: { preventDefault: () => void; stopPropagation: () => void; }) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+      }, false);
+    })();
   }
 
-}
+} 
