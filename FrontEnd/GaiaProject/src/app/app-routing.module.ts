ec580e476157725225fe2c58ef240e0908810c0b
leo-nardow <leonardoalmeida2703@gmail.com>
2021-10-04 15:23:31 -0300

    [Feature Ju: Task - Set the new routing]

--- FrontEnd/GaiaProject/src/app/app-routing.module.ts
+++ FrontEnd/GaiaProject/src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 
+
 const routes: Routes = [
-  {path:'', redirectTo:'home', pathMatch: 'full'},
+  {path:'', redirectTo:'login', pathMatch: 'full'},
   {path: 'home', component:HomeComponent},
   {path: 'aboutUs', component:AboutUsComponent},
   {path: 'contact', component:ContactComponent},
   {path: 'footer', component:FooterComponent},
   {path: 'menu', component:MenuComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'cadastro', component:CadastroComponent}
 ];
 
 @NgModule({
--- FrontEnd/GaiaProject/src/app/app.module.ts
+++ FrontEnd/GaiaProject/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutUsComponent } from './about-us/about-us.component';
@@ -11,6 +12,7 @@ import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+
 
 @NgModule({
   declarations: [
@@ -26,7 +28,8 @@ import { CadastroComponent } from './cadastro/cadastro.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
