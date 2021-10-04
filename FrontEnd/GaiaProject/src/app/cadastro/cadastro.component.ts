71168a9531da3702db37941bfb4eb2b525fca124
leo-nardow <leonardoalmeida2703@gmail.com>
2021-10-04 15:20:21 -0300

    [Feature Doug: Task -  Creating cadastro page]

--- FrontEnd/GaiaProject/src/app/cadastro/cadastro.component.ts
+++ FrontEnd/GaiaProject/src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario
+  senhaConfirma: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+
+  confirmarSenha(event: any) {
+    this.senhaConfirma = event.target.value
+  }
+
+  cadastrar() {
+    if (this.usuario.senha != this.senhaConfirma){
+      alert('As senhas estão incorretas')
+    } else {
+      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) =>{
+        this.usuario = resp
+        this.router.navigate(['/login'])
+        alert('Usuário cadastrado com sucesso!')
+      }) 
+    }
+
   }
 
 }
